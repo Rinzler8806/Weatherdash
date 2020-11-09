b62b67d6d2f158e64aed0eab6e7cf3fb3a73cd14
Rinzler8806 <shane.rogers916@gmail.com>
2020-11-08 16:34:51 -0800

    Updates

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var wind = document.querySelector('.wind');
+var humidity = document.querySelector('.humidity');
 var button= document.querySelector('.submit');
 var uv=document.querySelector('.uv');
 var uvI=document.querySelector('.uvI');
@@ -25,12 +26,13 @@ function currentWeather(city){
   var nameValue = data['name'];
   var descValue = data['weather'][0]['description'];
   var windValue = data['wind']['speed'];
+  var humidityValue = data['main']['humidity'];
 
-  main.innerHTML = nameValue;
-  desc.innerHTML = "Conditions - "+descValue;
-  temp.innerHTML = "Temperature - "+tempValue;
+    main.innerHTML = nameValue;
+    desc.innerHTML = "Conditions - "+descValue;
+    temp.innerHTML = "Temperature - "+tempValue;
      wind.innerHTML = "Wind Speed - "+windValue;
-
+    humidity.innerHTML = "Humidity - " +humidityValue;
 })
 }
 
@@ -46,6 +48,19 @@ function uvIndex(lat,lon){
     uvI.textContent=data.value;
 })
 }
+
+// function uvIndex(lat,lon){
+//     fetch('https://api.openweathermap.org/data/2.5/uvi?lat='+lat+'&lon='+lon+'&appid=ebcc3fd8dd47c4d2aaf75afc3bb3381b&units=imperial')
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data)
+//     uv.innerHTML ="UV Index - "+data.value;
+//     if (data.value>3){
+//         uv.setAttribute("class","green")
+//     }
+//     uvI.textContent=data.value;
+// })
+// }
 
 function forecast(city){
     fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ city +'&appid=ebcc3fd8dd47c4d2aaf75afc3bb3381b&units=imperial')
